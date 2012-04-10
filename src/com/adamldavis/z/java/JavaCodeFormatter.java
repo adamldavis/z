12e023e4192a09253e9001ea5f3bf1d4e49c95cd
Adam Davis <ada.m.d.a.v@gmail.com>
2012-04-10 16:02:40 -0400

    Updated JavaCodeFormatter to remove leading whitespace

--- src/com/adamldavis/z/java/JavaCodeFormatter.java
+++ src/com/adamldavis/z/java/JavaCodeFormatter.java
@@ -6,6 +6,14 @@ public class JavaCodeFormatter implements CodeFormatter {
 
 	@Override
 	public String format(String code) {
+		int slength = 0;
+		for (; code.charAt(slength) == ' ' || code.charAt(slength) == '\t'; slength++)
+			;
+
+		if (slength > 1) {
+			return code.replace(code.substring(0, slength), "");
+		}
+
 		return code;
 	}
 
