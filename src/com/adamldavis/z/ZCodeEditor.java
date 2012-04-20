431c2111b41283fbf6fc5b03fb720767000022a8
Adam Davis <ada.m.d.a.v@gmail.com>
2012-04-20 08:01:09 -0400

    Show neoedit help page when F1 is pressed in editor

--- src/com/adamldavis/z/ZCodeEditor.java
+++ src/com/adamldavis/z/ZCodeEditor.java
@@ -3,46 +3,52 @@ package com.adamldavis.z;
 import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
+import neoe.ne.EditorAdapter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import neoe.ne.TopCoderEditorPlugin;
-
+import com.adamldavis.swing.Swutil;
 import com.adamldavis.z.api.APIFactory;
+import com.adamldavis.z.api.ColorMode;
+import com.adamldavis.z.api.Editor;
 
 public class ZCodeEditor extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
-	private static final Logger log = LoggerFactory.getLogger(ZCodeEditor.class);
 
-	final JPanel editor;
+	private static final Logger log = LoggerFactory
+			.getLogger(ZCodeEditor.class);
+
+	final JPanel panel;
 
 	private ZNode zNode;
 
 	private final APIFactory apiFactory;
-	
-	private final TopCoderEditorPlugin plugin = new TopCoderEditorPlugin();
+
+	private final Editor editor = new EditorAdapter();
 
 	public ZCodeEditor(ZNode zNode, APIFactory apiFactory) {
 		super("Z code editor:" + zNode);
-		editor = plugin.getEditorPanel(); 
-		plugin.setSource(apiFactory.getCodeFormatter()
-				.format(zNode.getCode()));
-		JScrollPane scrollPane = new JScrollPane(editor);
+		panel = editor.getEditorPanel();
+		editor.applyColorMode(ColorMode.WHITE);
+		editor.setText(apiFactory.getCodeFormatter().format(zNode.getCode()));
+		JScrollPane scrollPane = new JScrollPane(panel);
 		this.getRootPane().setLayout(new BorderLayout());
 		this.getRootPane().add(scrollPane);
 		this.setVisible(true);
 		this.zNode = zNode;
 		this.apiFactory = apiFactory;
-		editor.requestFocus();
-		editor.addKeyListener(new KeyListener() {
+		panel.requestFocus();
+		panel.addKeyListener(new KeyListener() {
 
 			@Override
 			public void keyTyped(KeyEvent e) {
@@ -57,14 +63,22 @@ public class ZCodeEditor extends JFrame {
 
 			@Override
 			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_F1) {
+					try {
+						showHelp();
+					} catch (IOException e1) {
+						log.error(e1.getMessage());
+					}
+				}
 			}
 		});
 	}
 
 	public void save() {
 		log.info("Saving: {}", zNode);
-		zNode.replaceCode(plugin.getSource());
+		zNode.replaceCode(editor.getText());
 		new ZCodeSaver(apiFactory).save(zNode);
+		Swutil.flashMessage(this, "Saved " + zNode.name);
 	}
 
 	protected void saveLater() {
@@ -78,5 +92,16 @@ public class ZCodeEditor extends JFrame {
 				save();
 			}
 		});
+	}
+
+	protected void showHelp() throws IOException {
+		log.info("showHelp called.");
+		try {
+			Swutil.showStaticPage(Thread.currentThread()
+					.getContextClassLoader().getResource("./neoedit.html")
+					.toURI(), this.getSize(), this.getLocation());
+		} catch (URISyntaxException e) {
+			log.error(e.getMessage());
+		}
 	}
 }
